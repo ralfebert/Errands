9f357567fd925822a3b04678973bc6a9f57156f6
Ralf Ebert <info@ralfebert.de>
2018-07-09 14:18:06 +0200

    cloudkit example

--- Errands/ErrandsTableViewController.swift
+++ Errands/ErrandsTableViewController.swift
@@ -1,34 +1,130 @@
+import CloudKit
 import UIKit
 
 struct Errand {
 
-    var name : String
+    fileprivate static let recordType = "Errand"
+    fileprivate static let keys = (name : "name")
+
+    var record : CKRecord
+
+    init(record : CKRecord) {
+        self.record = record
+    }
+
+    init() {
+        self.record = CKRecord(recordType: Errand.recordType)
+    }
+
+    var name : String {
+        get {
+            return self.record.value(forKey: Errand.keys.name) as! String
+        }
+        set {
+            self.record.setValue(newValue, forKey: Errand.keys.name)
+        }
+    }
 
 }
 
 class ErrandsModel {
 
-    var errands = [Errand(name: "Apple")] {
+    private let database = CKContainer.default().privateCloudDatabase
+
+    var errands = [Errand]() {
         didSet {
-            self.onChange?()
+            self.notificationQueue.addOperation {
+                self.onChange?()
+            }
         }
     }
 
     var onChange : (() -> Void)?
     var onError : ((Error) -> Void)?
+    var notificationQueue = OperationQueue.main
+
+    var records = [CKRecord]()
+    var insertedObjects = [Errand]()
+    var deletedObjectIds = Set<CKRecordID>()
 
     init() {
     }
 
     func addErrand(name : String) {
-        self.errands.append(Errand(name: name))
+
+        var errand = Errand()
+        errand.name = name
+        database.save(errand.record) { _, error in
+            guard error == nil else {
+                self.handle(error: error!)
+                return
+            }
+        }
+
+        self.insertedObjects.append(errand)
+        self.updateErrands()
+
     }
 
     func delete(at index : Int) {
-        self.errands.remove(at: index)
+        let recordId = self.errands[index].record.recordID
+        database.delete(withRecordID: recordId) { _, error in
+            guard error == nil else {
+                self.handle(error: error!)
+                return
+            }
+        }
+
+        deletedObjectIds.insert(recordId)
+        updateErrands()
+    }
+
+    fileprivate func handle(error: Error) {
+        self.notificationQueue.addOperation {
+            self.onError?(error)
+        }
+    }
+
+    fileprivate func updateErrands() {
+
+        var knownIds = Set(records.map { $0.recordID })
+
+        // remove objects from our local list once we see them returned from the cloudkit storage
+        self.insertedObjects.removeAll { errand in
+            knownIds.contains(errand.record.recordID)
+        }
+        knownIds.formUnion(self.insertedObjects.map { $0.record.recordID })
+
+        // remove objects from our local list once we see them not being returned from storage anymore
+        self.deletedObjectIds.formIntersection(knownIds)
+
+        var errands = records.map { record in Errand(record: record) }
+
+        errands.append(contentsOf: self.insertedObjects)
+        errands.removeAll { errand in
+            deletedObjectIds.contains(errand.record.recordID)
+        }
+
+        self.errands = errands
+
+        debugPrint("Tracking local objects \(self.insertedObjects) \(self.deletedObjectIds)")
     }
 
     @objc func refresh() {
+
+        let query = CKQuery(recordType: Errand.recordType, predicate: NSPredicate(value: true))
+
+        database.perform(query, inZoneWith: nil) { records, error in
+            guard let records = records, error == nil else {
+                self.handle(error: error!)
+                return
+            }
+
+            self.records = records
+
+            self.updateErrands()
+        }
+
     }
 
 }
